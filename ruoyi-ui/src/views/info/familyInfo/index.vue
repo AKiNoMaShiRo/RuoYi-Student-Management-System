514f1b5fe68150e847cc9b71e38ed46a2a364ef8
akino_mashiro <akino_mashiro@qq.com>
2020-11-11 18:25:24 +0800

    学生信息管理页面优化

--- ruoyi-ui/src/views/info/familyInfo/index.vue
+++ ruoyi-ui/src/views/info/familyInfo/index.vue
@@ -1,95 +1,194 @@
 <template>
   <div class="app-container">
-    <el-form ref="infoForm" :model="formData" :inline="true">
-      <el-form-item label="称谓" prop="appellation">
-        <el-input v-model="formData.appellation"></el-input>
-      </el-form-item>
-      <el-form-item label="姓名" prop="name">
-        <el-input v-model="formData.name"></el-input>
-      </el-form-item>
-      <el-form-item label="身份证" prop="identityCard">
-        <el-input v-model="formData.identityCard"></el-input>
-      </el-form-item>
-      <el-form-item label="健康状况" prop="health">
-        <el-select v-model="formData.health">
-          <el-option
-            v-for="healthState in healthStates"
-            :key="healthState.v"
-            :label="healthState.l"
-            :value="healthState.v"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="单位名称" prop="company">
-        <el-input v-model="formData.company"></el-input>
-      </el-form-item>
-      <el-form-item label="职位" prop="duty">
-        <el-input v-model="formData.duty"></el-input>
-      </el-form-item>
-      <el-form-item label="邮编" prop="postCode">
-        <el-input v-model="formData.postCode"></el-input>
-      </el-form-item>
-      <el-form-item label="电话号码" prop="phoneNumber">
-        <el-input v-model="formData.phoneNumber"></el-input>
-      </el-form-item>
-      <el-form-item label="政治面貌" prop="politicsStatus">
-        <el-select v-model="formData.politicsStatus">
-          <el-option
-            v-for="politicsStatusOption in politicsStatusOptions"
-            :key="politicsStatusOption.v"
-            :label="politicsStatusOption.l"
-            :value="politicsStatusOption.v"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="备注" prop="comment">
-        <el-input v-model="formData.comment" type="textarea"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button disabsled @click="resetForm">重置</el-button>
-        <el-button type="primary" @click="submitForm">提交</el-button>
-      </el-form-item>
-    </el-form>
-    <el-table
-      height="300px"
-     :data="tableData"
-     style="height:100%"
-    >
-      <el-table-column
-        v-for="column in tableColumns"
-        v-bind="column"
-        :key="column.prop"
-        showOverflowTooltip
+    <section class="am-inline-form am-mb">
+      <div class="am-p">
+        <span>添加家庭成员信息</span>
+      </div>
+      <el-form ref="infoForm" :model="formData" :inline="true">
+        <el-row type="flex" justify="center">
+          <el-col :span="40" :xs="24">
+            <el-form-item label="称谓" prop="appellation" label-width="180px">
+              <el-input v-model="formData.appellation"></el-input>
+            </el-form-item>
+            <el-form-item label="姓名" prop="name" label-width="180px">
+              <el-input v-model="formData.name"></el-input>
+            </el-form-item>
+            <el-form-item label="身份证" prop="identityCard" label-width="180px">
+              <el-input v-model="formData.identityCard"></el-input>
+            </el-form-item>
+            <el-form-item label="健康状况" prop="health" label-width="180px">
+              <el-select v-model="formData.health">
+                <el-option
+                  v-for="healthState in healthStates"
+                  :key="healthState.v"
+                  :label="healthState.l"
+                  :value="healthState.v"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="单位名称" prop="company" label-width="180px">
+              <el-input v-model="formData.company"></el-input>
+            </el-form-item>
+            <el-form-item label="职位" prop="duty" label-width="180px">
+              <el-input v-model="formData.duty"></el-input>
+            </el-form-item>
+            <el-form-item label="邮编" prop="postCode" label-width="180px">
+              <el-input v-model="formData.postCode"></el-input>
+            </el-form-item>
+            <el-form-item label="电话号码" prop="phoneNumber" label-width="180px">
+              <el-input v-model="formData.phoneNumber"></el-input>
+            </el-form-item>
+            <el-form-item label="政治面貌" prop="politicsStatus" label-width="180px">
+              <el-select v-model="formData.politicsStatus">
+                <el-option
+                  v-for="politicsStatusOption in politicsStatusOptions"
+                  :key="politicsStatusOption.v"
+                  :label="politicsStatusOption.l"
+                  :value="politicsStatusOption.v"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="space-between">
+          <el-col :span="40" :xs="24">
+            <el-form-item label="备注" prop="comment" label-width="180px">
+              <div class="am-textarea-sb">
+                <el-input
+                  v-model="formData.comment"
+                  type="textarea"
+                  resize="none"
+                  :rows="3"
+                >
+                </el-input>
+              </div>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="space-between">
+          <el-col :span="40" :xs="24">
+            <el-form-item label=" " label-width="180px" >
+              <el-button disabsled @click="resetForm">重置</el-button>
+              <el-button type="primary" @click="submitForm">提交</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </section>
+    <section class="am-mb am-mt">
+      <div class="am-p">
+        <span>家庭成员信息</span>
+      </div>
+      <el-table
+        height="300px"
+      :data="tableData"
+      style="height:100%"
       >
-        <template slot-scope="scope">
-          {{ scope.row[column.prop] | dataFormatter(column.prop) }}
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" min-width="120">
-        <template slot-scope="scope">
-          <el-button
-            type="text"
-            icon="el-icon-edit"
-            size="mini"
-            @click="handleEdit(scope.row)"
-          >
-          编辑</el-button>
-          <el-button
-            type="text"
-            icon="el-icon-delete"
-            size="mini"
-            @click="handleDelete(scope.row)"
-          >
-          删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column
+          v-for="column in tableColumns"
+          v-bind="column"
+          :key="column.prop"
+          showOverflowTooltip
+        >
+          <template slot-scope="scope">
+            {{ scope.row[column.prop] | dataFormatter(column.prop) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" min-width="140" fixed="right">
+          <template slot-scope="scope">
+            <el-button
+              type="text"
+              icon="el-icon-edit"
+              size="mini"
+              @click="handleEdit(scope.row)"
+            >
+            编辑</el-button>
+            <el-button
+              type="text"
+              icon="el-icon-delete"
+              size="mini"
+              @click="handleDelete(scope.row)"
+            >
+            删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </section>
+    <el-dialog title="家庭成员信息详情" :visible.sync="dgFromVisible" class="am-dialog-form">
+      <el-form :model="dialogFormData" inline>
+        <el-row type="flex" justify="space-between">
+          <el-col :span="40">
+            <el-form-item label="称谓" prop="appellation" label-width="100px">
+              <el-input v-model="dialogFormData.appellation"></el-input>
+            </el-form-item>
+            <el-form-item label="姓名" prop="name" label-width="100px">
+              <el-input v-model="dialogFormData.name"></el-input>
+            </el-form-item>
+            <el-form-item label="身份证" prop="identityCard" label-width="100px">
+              <el-input v-model="dialogFormData.identityCard"></el-input>
+            </el-form-item>
+            <el-form-item label="健康状况" prop="health" label-width="100px">
+              <el-select v-model="dialogFormData.health">
+                <el-option
+                  v-for="healthState in healthStates"
+                  :key="healthState.v"
+                  :label="healthState.l"
+                  :value="healthState.v"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="单位名称" prop="company" label-width="100px">
+              <el-input v-model="dialogFormData.company"></el-input>
+            </el-form-item>
+            <el-form-item label="职位" prop="duty" label-width="100px">
+              <el-input v-model="dialogFormData.duty"></el-input>
+            </el-form-item>
+            <el-form-item label="邮编" prop="postCode" label-width="100px">
+              <el-input v-model="dialogFormData.postCode"></el-input>
+            </el-form-item>
+            <el-form-item label="电话号码" prop="phoneNumber" label-width="100px">
+              <el-input v-model="dialogFormData.phoneNumber"></el-input>
+            </el-form-item>
+            <el-form-item label="政治面貌" prop="politicsStatus" label-width="100px">
+              <el-select v-model="dialogFormData.politicsStatus">
+                <el-option
+                  v-for="politicsStatusOption in politicsStatusOptions"
+                  :key="politicsStatusOption.v"
+                  :label="politicsStatusOption.l"
+                  :value="politicsStatusOption.v"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="40">
+            <el-form-item label="备注" prop="comment" label-width="100px">
+              <el-input
+                v-model="dialogFormData.comment"
+                type="textarea"
+                resize="none"
+                :rows="3"
+              >
+              </el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <div slot="footer" class="dialog-footer" label-width="100px">
+        <el-button @click="handleDialogFormCancel">取消</el-button>
+        <el-button type="primary" @click="handleDialogFormConform">确定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapState } from "vuex";
 import { politicsStatusOptions, healthStates } from '../formOption'
 import { provinceAndCityData, regionData, provinceAndCityDataPlus, regionDataPlus, CodeToText, TextToCode } from 'element-china-area-data'
 
@@ -99,7 +198,20 @@ export default {
     return {
       politicsStatusOptions: politicsStatusOptions,
       healthStates: healthStates,
+      dgFromVisible: false,
       formData: {
+        appellation: '',
+        name: '',
+        identityCard: '',
+        health: '',
+        company: '',
+        duty: '',
+        postCode: '',
+        phoneNumber: '',
+        politicsStatus: '',
+        comment: ''
+      },
+      dialogFormData: {
         appellation: '',
         name: '',
         identityCard: '',
@@ -114,14 +226,14 @@ export default {
       tableColumns: Object.freeze([
         { label: '称谓', prop: 'appellation', minWidth: '80' },
         { label: '姓名', prop: 'name', minWidth: '80' },
-        { label: '身份证', prop: 'identityCard', minWidth: '180' },
-        { label: '健康状况', prop: 'health', minWidth: '100' },
-        { label: '单位名称', prop: 'company', minWidth: '140' },
-        { label: '职务', prop: 'duty', minWidth: '100' },
-        { label: '邮编', prop: 'postCode', minWidth: '80' },
+        { label: '身份证', prop: 'identityCard', minWidth: '120' },
+        { label: '健康状况', prop: 'health', minWidth: '80' },
+        { label: '单位名称', prop: 'company', minWidth: '120' },
+        { label: '职务', prop: 'duty', minWidth: '80' },
+        { label: '邮编', prop: 'postCode', minWidth: '70' },
         { label: '电话号码', prop: 'phoneNumber', minWidth: '120' },
         { label: '政治面貌', prop: 'politicsStatus', minWidth: '120' },
-        { label: '备注', prop: 'comment', minWidth: '180' },
+        { label: '备注', prop: 'comment', minWidth: '140' },
       ]),
       tableData: [
         {
@@ -139,6 +251,12 @@ export default {
       ]
     }
   },
+  computed: {
+    ...mapGetters(['sidebar']),
+    isSideBar () {
+      return this.sidebar.open
+    }
+  },
   methods: {
     resetForm () {
       this.$refs.infoForm.resetFields()
@@ -146,8 +264,16 @@ export default {
     submitForm () {
     },
     handleEdit (row) {
+      this.dgFromVisible = true
+      this.dialogFormData = row
     },
     handleDelete (row) {
+    },
+    handleDialogFormCancel () {
+      this.dgFromVisible = false
+    },
+    handleDialogFormConform () {
+      this.dgFromVisible = false
     }
   },
   filters: {
--- ruoyi-ui/src/views/info/studentInfo/index.vue
+++ ruoyi-ui/src/views/info/studentInfo/index.vue
@@ -1,163 +1,153 @@
 <template>
   <div class="app-container">
-    <el-form ref="infoForm" :model="formData" :inline="true">
-      <el-form-item label="院系" prop="department">
-        <div></div>
-        <el-input v-model="formData.department" disabled></el-input>
-      </el-form-item>
-      <el-form-item label="班级" prop="class">
-        <div></div>
-        <el-input v-model="formData.class" disabled></el-input>
-      </el-form-item>
-      <el-form-item label="学号" prop="number">
-        <div></div>
-        <el-input v-model="formData.number" disabled></el-input>
-      </el-form-item>
-      <el-form-item label="姓名" prop="name">
-        <div></div>
-        <el-input v-model="formData.name" disabled></el-input>
-      </el-form-item>
-      <el-form-item label="毕业意向调查" prop="graduateIntention">
-        <div></div>
-        <el-select v-model="formData.graduateIntention">
-          <el-option
-            v-for="graduateIntentionType in graduateIntentionTypes"
-            :key="graduateIntentionType.v"
-            :label="graduateIntentionType.l"
-            :value="graduateIntentionType.v"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="是否已缴费注册" prop="registration">
-        <div></div>
-        <el-select v-model="formData.registration">
-          <el-option
-            v-for="registrationState in registrationStates"
-            :key="registrationState.v"
-            :label="registrationState.l"
-            :value="registrationState.v"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="导师姓名" prop="teacher">
-        <div></div>
-        <el-input v-model="formData.teacher"></el-input>
-      </el-form-item>
-      <el-form-item label="手机" prop="phoneNumber">
-        <div></div>
-        <el-input v-model="formData.phoneNumber"></el-input>
-      </el-form-item>
-      <el-form-item label="电子邮箱" prop="email">
-        <div></div>
-        <el-input v-model="formData.email"></el-input>
-      </el-form-item>
-      <el-form-item label="QQ" prop="QQ">
-        <div></div>
-        <el-input v-model="formData.QQ"></el-input>
-      </el-form-item>
-      <el-form-item label="微信" prop="weChat">
-        <div></div>
-        <el-input v-model="formData.weChat"></el-input>
-      </el-form-item>
-      <el-form-item label="出生日期" prop="birthday">
-        <div></div>
-        <el-date-picker v-model="formData.birthday" type="date"></el-date-picker>
-      </el-form-item>
-      <el-form-item label="民族" prop="nation">
-        <div></div>
-        <el-select v-model="formData.nation">
-          <el-option
-            v-for="(nation, index) in nationNames"
-            :key="index"
-            :label="nation"
-            :value="String(index)"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="政治面貌" prop="politicsStatus">
-        <div></div>
-        <el-select v-model="formData.politicsStatus">
-          <el-option
-            v-for="politicsStatusOption in politicsStatusOptions"
-            :key="politicsStatusOption.v"
-            :label="politicsStatusOption.l"
-            :value="politicsStatusOption.v"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="加入时间" prop="joinTime">
-        <div></div>
-        <el-date-picker v-model="formData.joinTime" type="month"></el-date-picker>
-      </el-form-item>
-      <el-form-item label="生源地区" prop="studentOrigin">
-        <div></div>
-        <el-cascader v-model="formData.studentOrigin" :options="regionData" @change="handleChange">
-        </el-cascader>
-      </el-form-item>
-      <el-form-item label="家庭地址" prop="familyAddress">
-        <div></div>
-        <el-input v-model="formData.familyAddress"></el-input>
-      </el-form-item>
-      <el-form-item label="家庭电话" prop="familyPhoneNumber">
-        <div></div>
-        <el-input v-model="formData.familyPhoneNumber"></el-input>
-      </el-form-item>
-      <el-form-item label="家庭联系人" prop="familyContacts">
-        <div></div>
-        <el-input v-model="formData.familyContacts"></el-input>
-      </el-form-item>
-      <el-form-item label="家庭邮编" prop="familyPostcode">
-        <div></div>
-        <el-input v-model="formData.familyPostcode"></el-input>
-      </el-form-item>
-      <el-form-item label="毕业中学" prop="graduateSchool">
-        <div></div>
-        <el-input v-model="formData.graduateSchool"></el-input>
-      </el-form-item>
-      <el-form-item label="中学邮编" prop="graduateSchoolPostcode">
-        <div></div>
-        <el-input v-model="formData.graduateSchoolPostcode"></el-input>
-      </el-form-item>
-      <el-form-item label="曾用名" prop="usedName">
-        <div></div>
-        <el-input v-model="formData.usedName"></el-input>
-      </el-form-item>
-      <el-form-item label="籍贯" prop="nativePlace">
-        <div></div>
-        <el-input v-model="formData.nativePlace"></el-input>
-      </el-form-item>
-      <el-form-item label="医保卡号" prop="healthCard">
-        <div></div>
-        <el-input v-model="formData.healthCard"></el-input>
-      </el-form-item>
-      <el-form-item label="四级成绩" prop="cet4">
-        <div></div>
-        <el-input v-model="formData.cet4"></el-input>
-      </el-form-item>
-      <el-form-item label="六级成绩" prop="cet6">
-        <div></div>
-        <el-input v-model="formData.cet6"></el-input>
-      </el-form-item>
-      <el-form-item label="英文名" prop="englishName">
-        <div></div>
-        <el-input v-model="formData.englishName"></el-input>
-      </el-form-item>
-      <el-form-item label="信息上报项目" prop="reportItem">
-        <div></div>
-        <el-select v-model="formData.reportItem">
-          <el-option label="建档立卡学生" value="1"></el-option>
-          <el-option label="非建档立卡学生" value="2"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button disabsled @click="resetForm">重置</el-button>
-        <el-button type="primary" @click="submitForm">提交</el-button>
-      </el-form-item>
-    </el-form>
+    <section class="am-inline-form">
+      <el-form ref="infoForm" :model="formData" :inline="true">
+        <el-row type="flex" justify="space-between" align="bottom">
+          <el-col :span="20" :xs="30">
+            <el-form-item label="院系" prop="department" label-width="180px">
+              <el-input v-model="formData.department" disabled></el-input>
+            </el-form-item>
+            <el-form-item label="班级" prop="class" label-width="180px">
+              <el-input v-model="formData.class" disabled></el-input>
+            </el-form-item>
+            <el-form-item label="学号" prop="number" label-width="180px">
+              <el-input v-model="formData.number" disabled></el-input>
+            </el-form-item>
+            <el-form-item label="姓名" prop="name" label-width="180px">
+              <el-input v-model="formData.name" disabled></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="10" :xs="30">
+            <div class="am-flex-center am-mb-lg am-mr" style="width: 360px">
+              <el-image class="stu-photo" style="width: 110px; height: 150px"></el-image>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="space-between">
+          <el-col :span="30" :xs="30">
+            <el-form-item label="毕业意向调查" prop="graduateIntention" label-width="180px">
+              <el-select v-model="formData.graduateIntention">
+                <el-option
+                  v-for="graduateIntentionType in graduateIntentionTypes"
+                  :key="graduateIntentionType.v"
+                  :label="graduateIntentionType.l"
+                  :value="graduateIntentionType.v"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="是否已缴费注册" prop="registration" label-width="180px">
+              <el-select v-model="formData.registration">
+                <el-option
+                  v-for="registrationState in registrationStates"
+                  :key="registrationState.v"
+                  :label="registrationState.l"
+                  :value="registrationState.v"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="导师姓名" prop="teacher" label-width="180px">
+              <el-input v-model="formData.teacher"></el-input>
+            </el-form-item>
+            <el-form-item label="手机号码" prop="phoneNumber" label-width="180px">
+              <el-input v-model="formData.phoneNumber"></el-input>
+            </el-form-item>
+            <el-form-item label="电子邮箱" prop="email" label-width="180px">
+              <el-input v-model="formData.email"></el-input>
+            </el-form-item>
+            <el-form-item label="QQ" prop="QQ" label-width="180px">
+              <el-input v-model="formData.QQ"></el-input>
+            </el-form-item>
+            <el-form-item label="微信" prop="weChat" label-width="180px">
+              <el-input v-model="formData.weChat"></el-input>
+            </el-form-item>
+            <el-form-item label="出生日期" prop="birthday" label-width="180px">
+              <el-date-picker v-model="formData.birthday" type="date"></el-date-picker>
+            </el-form-item>
+            <el-form-item label="民族" prop="nation" label-width="180px">
+              <el-select v-model="formData.nation">
+                <el-option
+                  v-for="(nation, index) in nationNames"
+                  :key="index"
+                  :label="nation"
+                  :value="String(index)"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="政治面貌" prop="politicsStatus" label-width="180px">
+              <el-select v-model="formData.politicsStatus">
+                <el-option
+                  v-for="politicsStatusOption in politicsStatusOptions"
+                  :key="politicsStatusOption.v"
+                  :label="politicsStatusOption.l"
+                  :value="politicsStatusOption.v"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="加入时间" prop="joinTime" label-width="180px">
+              <el-date-picker v-model="formData.joinTime" type="month"></el-date-picker>
+            </el-form-item>
+            <el-form-item label="生源地区" prop="studentOrigin" label-width="180px">
+              <el-cascader v-model="formData.studentOrigin" :options="regionData" @change="handleChange">
+              </el-cascader>
+            </el-form-item>
+            <el-form-item label="家庭地址" prop="familyAddress" label-width="180px">
+              <el-input v-model="formData.familyAddress"></el-input>
+            </el-form-item>
+            <el-form-item label="家庭电话" prop="familyPhoneNumber" label-width="180px">
+              <el-input v-model="formData.familyPhoneNumber"></el-input>
+            </el-form-item>
+            <el-form-item label="家庭联系人" prop="familyContacts" label-width="180px">
+              <el-input v-model="formData.familyContacts"></el-input>
+            </el-form-item>
+            <el-form-item label="家庭邮编" prop="familyPostcode" label-width="180px">
+              <el-input v-model="formData.familyPostcode"></el-input>
+            </el-form-item>
+            <el-form-item label="毕业中学" prop="graduateSchool" label-width="180px">
+              <el-input v-model="formData.graduateSchool"></el-input>
+            </el-form-item>
+            <el-form-item label="中学邮编" prop="graduateSchoolPostcode" label-width="180px">
+              <el-input v-model="formData.graduateSchoolPostcode"></el-input>
+            </el-form-item>
+            <el-form-item label="曾用名" prop="usedName" label-width="180px">
+              <el-input v-model="formData.usedName"></el-input>
+            </el-form-item>
+            <el-form-item label="籍贯" prop="nativePlace" label-width="180px">
+              <el-cascader v-model="formData.nativePlace" :options="regionData"></el-cascader>
+            </el-form-item>
+            <el-form-item label="医保卡号" prop="healthCard" label-width="180px">
+              <el-input v-model="formData.healthCard"></el-input>
+            </el-form-item>
+            <el-form-item label="四级成绩" prop="cet4" label-width="180px">
+              <el-input v-model="formData.cet4"></el-input>
+            </el-form-item>
+            <el-form-item label="六级成绩" prop="cet6" label-width="180px">
+              <el-input v-model="formData.cet6"></el-input>
+            </el-form-item>
+            <el-form-item label="英文名" prop="englishName" label-width="180px">
+              <el-input v-model="formData.englishName"></el-input>
+            </el-form-item>
+            <el-form-item label="信息上报项目" prop="reportItem" label-width="180px">
+              <el-select v-model="formData.reportItem">
+                <el-option label="建档立卡学生" value="1"></el-option>
+                <el-option label="非建档立卡学生" value="2"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="space-between">
+          <el-col :span="30" :xs="30">
+            <el-form-item label=" " label-width="180px">
+              <el-button disabsled @click="resetForm">重置</el-button>
+              <el-button type="primary" @click="submitForm">提交</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </section>
   </div>
 </template>
 
@@ -219,3 +209,10 @@ export default {
   }
 }
 </script>
+
+<style>
+.stu-photo{
+  border-radius: 4px;
+  border: 1px solid #dfe4ed;
+}
+</style>
